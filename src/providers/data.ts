796dba1c6b6d5f9d506b4a4b2bc0b3c0e640ee05
Jie-Si Chen <jessie@paxai.com>
2026-01-23 15:42:37 +0800

    init commit

--- src/providers/data.ts
+++ src/providers/data.ts
@@ -1,24 +1,20 @@
-import { createSimpleRestDataProvider } from "@refinedev/rest/simple-rest";
-import { API_URL } from "./constants";
-import {DataProvider, GetListParams, BaseRecord, GetListResponse} from "@refinedev/core"
-import { mockSubjects } from "./mockData";
-
-// export const { dataProvider, kyInstance } = createSimpleRestDataProvider({
-//   apiURL: API_URL,
-// });
-export const dataProvider: DataProvider = {
-  getList: async <TData extends BaseRecord = BaseRecord>({resource}: GetListParams): Promise<GetListResponse<TData>> => {
-    if(resource !== 'subjects'){
-      return {data: [] as TData[], total: 0};
+import { BACKEND_BASE_URL } from "@/constants"
+import { ListResponse } from "@/types";
+import {createDataProvider, CreateDataProviderOptions} from "@refinedev/rest"
+import { resourceLimits } from "worker_threads";
+const options: CreateDataProviderOptions = {
+  getList: {
+    getEndpoint: ({resource}) => resource,
+    mapResponse: async(response) => {
+      const payload: ListResponse = await response.json();
+      return payload.data ?? [];
+    },
+    getTotalCount: async(response) => {
+      const payload: ListResponse = await response.clone().json();
+      return payload.pagination?.total ?? payload.data?.length ?? 0;
     }
-    return {
-      data: mockSubjects as unknown as TData[],
-      total: mockSubjects.length
-    }
-  },
-  getOne: async () => {throw new Error('This function is not present in mock')},
-  create: async () => {throw new Error('This function is not present in mock')},
-  update: async () => {throw new Error('This function is not present in mock')},
-  deleteOne: async () => {throw new Error('This function is not present in mock')},
-  getApiUrl: () =>  ''
+  }
 }
+const {dataProvider} = createDataProvider(BACKEND_BASE_URL, options);
+
+export {dataProvider};
