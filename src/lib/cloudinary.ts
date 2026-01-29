a690e9a557bfd0013ff2018c3e239c4d8c5adf20
Jie-Si Chen <jessie@paxai.com>
2026-01-29 15:12:28 +0800

    implement class details

---
+++ src/lib/cloudinary.ts
@@ -0,0 +1,25 @@
+import { CLOUDINARY_CLOUD_NAME } from "@/constants"
+import { fill } from "@cloudinary/url-gen/actions/resize"
+import {Cloudinary} from "@cloudinary/url-gen"
+import { text } from "@cloudinary/url-gen/qualifiers/source"
+import { dpr, format, quality } from "@cloudinary/url-gen/actions/delivery"
+import { source } from "@cloudinary/url-gen/actions/overlay";
+import { TextStyle } from "@cloudinary/url-gen/qualifiers/textStyle"
+import { Position } from "@cloudinary/url-gen/qualifiers"
+import { compass } from "@cloudinary/url-gen/qualifiers/gravity"
+const cld = new Cloudinary({cloud: {cloudName: CLOUDINARY_CLOUD_NAME}})
+
+export const bannerPhoto = (imageCldPubId: string, name: string) => {
+    return cld
+.image(imageCldPubId)
+.resize(fill())
+.delivery(format('auto'))
+.delivery(quality('auto'))
+.delivery(dpr('auto'))
+.overlay(source(text(name, new TextStyle('roboto', 100).fontWeight('bold'))
+.textColor('white')).position(new Position()
+.gravity(compass('west'))
+.offsetX(0.02))
+)
+
+}
